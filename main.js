ffebe6f0e9867aba462e9e31840e03f2f31059d6
domingo79 <andria140779@yahoo.it>
2021-04-27 11:06:40 +0200

    ok

---
+++ main.js
@@ -0,0 +1,13 @@
+// L’utente inserisce due parole in successione, con due prompt.
+var parola_1 = prompt('inserisci una parola');
+var parola_2 = prompt('inserisci un\'altra parola');
+
+
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+if (parola_1 > parola_2) {
+    console.log(parola_1 + ' ' + parola_2);
+} else if (parola_2 > parola1) {
+    console.log(parola_2 + ' ' + parola_1);
+} else {
+    console.log('sembrano uguali');
+}
