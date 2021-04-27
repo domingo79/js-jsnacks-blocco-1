24530234b99261a0bd98f1b4938aa1cb0829cbeb
domingo79 <andria140779@yahoo.it>
2021-04-27 11:45:44 +0200

    es 2

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 // L’utente inserisce due parole in successione, con due prompt.
-var parola_1 = prompt('inserisci una parola');
-var parola_2 = prompt('inserisci un\'altra parola');
+// var parola_1 = prompt('inserisci una parola');
+// var parola_2 = prompt('inserisci un\'altra parola');
 
 //console.log(parola_1, parola_2);
 // Il software stampa prima la parola più corta, poi la parola più lunga.
-if (parola_1.length === parola_2.length) {
-    console.log('Riprova sembrano uguali');
-} else if (parola_1.length > parola_2.length) {
-    console.log(parola_1 + ' ' + parola_2);
-} else {
-    console.log(parola_2 + ' ' + parola_1);
+// if (parola_1.length === parola_2.length) {
+//     console.log('Riprova sembrano uguali');
+// } else if (parola_1.length > parola_2.length) {
+//     console.log(parola_1 + ' ' + parola_2);
+// } else {
+//     console.log(parola_2 + ' ' + parola_1);
+// }
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti. (modificato) 
+
+var somma = numeri++;
+for (var i = 0; i < 10; i++) {
+    var numeri = prompt('inserisci un numero');
 }
+console.log(numeri);
+console.log(somma);
