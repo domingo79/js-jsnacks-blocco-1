1d866ef605b520961ee701f28a4f4e93f651b33c
domingo79 <andria140779@yahoo.it>
2021-04-27 11:36:11 +0200

    mod

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 var parola_1 = prompt('inserisci una parola');
 var parola_2 = prompt('inserisci un\'altra parola');
 
-
+//console.log(parola_1, parola_2);
 // Il software stampa prima la parola più corta, poi la parola più lunga.
-if (parola_1 > parola_2) {
+if (parola_1.length === parola_2.length) {
+    console.log('Riprova sembrano uguali');
+} else if (parola_1.length > parola_2.length) {
     console.log(parola_1 + ' ' + parola_2);
-} else if (parola_2 > parola_1) {
-    console.log(parola_2 + ' ' + parola_1);
 } else {
-    console.log('sembrano uguali');
+    console.log(parola_2 + ' ' + parola_1);
 }
