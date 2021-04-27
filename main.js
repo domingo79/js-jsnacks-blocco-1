deedb77a56907c5b5f5cc9ca49334a55af86c5f1
domingo79 <andria140779@yahoo.it>
2021-04-27 11:13:38 +0200

    prova

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var parola_2 = prompt('inserisci un\'altra parola');
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 if (parola_1 > parola_2) {
     console.log(parola_1 + ' ' + parola_2);
-} else if (parola_2 > parola1) {
+} else if (parola_2 > parola_1) {
     console.log(parola_2 + ' ' + parola_1);
 } else {
     console.log('sembrano uguali');
