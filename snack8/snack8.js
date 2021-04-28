d2625700a22347f66ef4de1ab4de9023e418c9e6
domingo79 <andria140779@yahoo.it>
2021-04-28 12:57:51 +0200

    4cifre

---
+++ snack8/snack8.js
@@ -0,0 +1,9 @@
+// Chiedi un numero di 4 cifre all’utente
+// e calcola la somma di tutte le cifre che compongono il numero. (modificato) 
+
+var numero = Number(prompt('numero di quattro cifre..'));
+
+for (var i = 0; i < numero.length; i++) {
+    var somma = numero[i];
+}
+console.log('il tuo numero è ' + numero + somma);
