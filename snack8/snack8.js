0dd0fd4e9dc510f6dcdba1d47d95e792df4b9f28
domingo79 <andria140779@yahoo.it>
2021-04-29 09:47:24 +0200

    corretto

--- snack8/snack8.js
+++ snack8/snack8.js
@@ -2,11 +2,16 @@
 // Chiedi un numero di 4 cifre all’utente
 var n = prompt('numero di 4 cifre?');
 
+
+var userNumber = n.split(' ');
+
 // e calcola la somma di tutte le cifre che compongono il numero. (modificato) 
 var somma = 0;
-for (var i = 0; i < n.length; i++) {
-    console.log(n[i]);
-    somma = parseInt(somma) + parseInt(n[i]);
-    console.log('ecco la somma: ' + somma);
+for (var i = 0; i < userNumber.length; i++) {
+    var ele = userNumber[i];
+    somma += Number(ele);
 }
+console.log(somma);
+
+
 
