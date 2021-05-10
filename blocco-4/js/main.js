e6f9240bb0f940b641ee208406868d92a47ccbb9
domingo79 <andria140779@yahoo.it>
2021-05-10 12:21:19 +0200

    triangolo

--- blocco-4/js/main.js
+++ blocco-4/js/main.js
@@ -5,7 +5,22 @@ var Palla = {
 }
 
 // snack 1b Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
-var nuovoPeso = parseInt(prompt('Inserisci il peso della palla'));
+//var nuovoPeso = parseInt(prompt('Inserisci il peso della palla'));
+//console.log(nuovoPeso);
+//Palla.peso = nuovoPeso;
+//console.log(Palla);
 
-Palla.peso = nuovoPeso;
-console.log(Palla);
+//Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base e altezza.
+//Calcolare perimetro e area.
+
+var Triangolo = {
+    b: 20,
+    a: 15,
+}
+var a = 15;
+var b = 20;
+var aria = (b * a) / 2;
+console.log(aria);
+var c = (Math.sqrt(a)) + (Math.sqrt(b));
+var perimetro = a + b + c;
+console.log(perimetro);
