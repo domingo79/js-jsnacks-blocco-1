f3b48341991ff8bfa7f25b60cbbda01c3150ff0a
domingo79 <andria140779@yahoo.it>
2021-05-10 11:54:23 +0200

    mod-peso

--- blocco-4/js/main.js
+++ blocco-4/js/main.js
@@ -1,6 +1,10 @@
-
+// snack 1a 
 var Palla = {
     nome: 'palla',
     peso: 10
 }
-console.log(Palla.nome, Palla.peso);
+
+// snack 1b Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
+var modPeso = parseInt(prompt('Inserisci il peso'));
+
+Palla.push({ peso: modPeso });
